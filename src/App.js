3840ae2fb215d9bf73e0193538e8e9ba65f2d403
Tukhik <tukhik_k@mail.ru>
2021-01-09 01:50:34 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import React from 'react';
 import './App.css';
 //import {Product} from './Product.jsx'
-import Input from './Input';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDo from './components/ToDo/ToDo';
 
 function App() {
   return (
     /*<div className="App">
        <Product defaultValue= {"5$"}  description = " Fresh bananas from Ecuador" name=" banabas"/>
     </div>*/
-    <div className="addItem">
+    <div className="App">
     <div>todo list</div>
-      <Input />
+      <ToDo />
       </div>  
   );
 }
---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,46 @@
+import react, {Component} from 'react';
+import '../../index.css';
+
+export default class ToDo extends Component {
+	state = {
+		text: " ",
+		inputValue: " ",
+		arr: []
+	};
+	handleChange = (event)=>{
+		this.setState({
+			inputValue: event.target.value
+		});
+	};
+	
+ 	
+	handleClick =()=>{
+		const inputValue = this.state.inputValue.trim();
+		if(!inputValue){
+			return
+		}
+		this.setState({
+			text: this.state.inputValue,
+			inputValue: "",
+			arr: [...this.state.arr, this.state.inputValue]
+		})
+	}
+ 	removeItem=(item)=>{
+        this.setState({arr:this.state.arr.splice(item,0)})
+       
+    };
+
+	render(){ 
+		return(<div className= 'addItem'>
+			<input value ={this.state.inputValue}
+			type = "text" 
+			onChange = {this.handleChange}/>
+			<button onClick = {this.handleClick}> Click me</button>
+			<ol>
+			 {this.state.arr.map(item => (
+            <li key={item} className= "liClass">{item}<button onClick = {this.removeItem}>Remove</button></li>
+          ))}
+			</ol>
+		</div>)
+	}
+}
