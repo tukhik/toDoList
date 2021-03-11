349a17310159c88565e65a073de996a9880236c9
Tukhik <tukhik_k@mail.ru>
2021-03-12 02:28:02 +0400

    modify some

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,11 @@ import reducer from './reducer';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const middleware = applyMiddleware(thunk, logger);
+const params = [thunk];
+if(process.env.NODE_ENV === "development"){
+    params.push(logger);
+}
+
+const middleware = applyMiddleware(...params);
 
 export const store = createStore(reducer, middleware);
